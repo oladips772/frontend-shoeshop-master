827f98cdc8546d975d53cd45cc6cc96c985e666d
oladips <oladips200@gmail.com>
2022-02-19 21:54:23 +0000

    created orderModel and product model

---
+++ server/Models/productModel.js
@@ -0,0 +1,66 @@
+/** @format */
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  rating: {
+    type: Number,
+    require: true,
+  },
+  comment: {
+    type: String,
+    require: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    default: 0,
+  },
+});
+
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    rating: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    reviews: [reviewSchema],
+    price: {
+      type: Number,
+      require: true,
+    },
+    numReviews: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
