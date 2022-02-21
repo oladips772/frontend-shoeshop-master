9213dd3079eebadaf23d0875c252c3b77e3047c4
oladips <oladips200@gmail.com>
2022-02-21 06:49:29 +0000

    added productaction

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -2,8 +2,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducer } from "./Reducers/ProductReducers";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList:productListReducer
+});
 
 const initialState = {};
 
