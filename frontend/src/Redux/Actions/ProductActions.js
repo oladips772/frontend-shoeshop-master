db230c2d86db1c1dc44ada9559bda37fb8142250
oladips <oladips200@gmail.com>
2022-02-21 11:58:35 +0000

    created product action and called products with it

--- frontend/src/Redux/Actions/ProductActions.js
+++ frontend/src/Redux/Actions/ProductActions.js
@@ -1,12 +1,12 @@
 /** @format */
+import axios from "axios";
 import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
 } from "../Constants/ProductConstants";
-import axios from "axios";
 
-export const listProduct = async (dispatch) => {
+export const listProduct = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
     const { data } = await axios.get("/api/products");
--- frontend/src/components/homeComponents/ShopSection.js
+++ frontend/src/components/homeComponents/ShopSection.js
@@ -1,21 +1,20 @@
 /** @format */
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Pagination from "./pagination";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProduct } from "../../Redux/Actions/ProductActions";
 
 const ShopSection = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+  console.log(products);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-      console.log(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProduct());
+  }, [dispatch]);
 
   return (
     <>
@@ -24,34 +23,42 @@ const ShopSection = () => {
           <div className="row">
             <div className="col-lg-12 col-md-12 article">
               <div className="shopcontainer row">
-                {products.map((product) => (
-                  <div
-                    className="shop col-lg-4 col-md-6 col-sm-6"
-                    key={product._id}
-                  >
-                    <div className="border-product">
-                      <Link to={`/products/${product._id}`}>
-                        <div className="shopBack">
-                          <img src={product.image} alt={product.name} />
-                        </div>
-                      </Link>
-
-                      <div className="shoptext">
-                        <p>
+                {loading ? (
+                  <p>loading .... </p>
+                ) : error ? (
+                  <p>Error : {error}</p>
+                ) : (
+                  <>
+                    {products.map((product) => (
+                      <div
+                        className="shop col-lg-4 col-md-6 col-sm-6"
+                        key={product._id}
+                      >
+                        <div className="border-product">
                           <Link to={`/products/${product._id}`}>
-                            {product.name}
+                            <div className="shopBack">
+                              <img src={product.image} alt={product.name} />
+                            </div>
                           </Link>
-                        </p>
 
-                        <Rating
-                          value={product.rating}
-                          text={`${product.numReviews} reviews`}
-                        />
-                        <h3>${product.price}</h3>
+                          <div className="shoptext">
+                            <p>
+                              <Link to={`/products/${product._id}`}>
+                                {product.name}
+                              </Link>
+                            </p>
+
+                            <Rating
+                              value={product.rating}
+                              text={`${product.numReviews} reviews`}
+                            />
+                            <h3>${product.price}</h3>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                ))}
+                    ))}
+                  </>
+                )}
                 {/* Pagination */}
                 <Pagination />
               </div>
