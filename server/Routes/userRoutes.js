cb1145bb5270b36404d7caa7c68acb12f8f2948f
oladips <oladips200@gmail.com>
2022-02-24 13:40:14 +0000

    done with login api and begining register api

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,9 +1,9 @@
 /** @format */
 import express from "express";
 import asyncHandler from "express-async-handler";
+import { protect } from "../Middleware/Authmiddleware.js";
 import User from "../Models/userModel.js";
 import generateToken from "../utils/generateToken.js";
-// import { protect, admin } from "../Middleware/AuthMiddleware.js";
 
 const userRoute = express.Router();
 
@@ -35,10 +35,9 @@ userRoute.get(
 // PROFILE
 userRoute.get(
   "/profile",
-  // protect,
+  protect,
   asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
-
     if (user) {
       res.json({
         _id: user._id,
@@ -50,6 +49,40 @@ userRoute.get(
     } else {
       res.status(404);
       throw new Error("User not found");
+    }
+  })
+);
+
+// REGISTER
+userRoute.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid User Data");
     }
   })
 );
