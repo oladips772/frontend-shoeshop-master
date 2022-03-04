b98864baab0a61ab4ee26e7cf8706d56627959aa
oladips <oladips200@gmail.com>
2022-03-04 12:06:50 +0000

    creating product reviews api

--- frontend/src/Redux/Reducers/ProductReducers.js
+++ frontend/src/Redux/Reducers/ProductReducers.js
@@ -6,8 +6,13 @@ import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_CREATE_REVIEWS_REQUEST,
+  PRODUCT_CREATE_REVIEWS_SUCCESS,
+  PRODUCT_CREATE_REVIEWS_FAIL,
+  PRODUCT_CREATE_REVIEWS_RESET,
 } from "../Constants/ProductConstants";
 
+// product listing
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
@@ -21,17 +26,33 @@ export const productListReducer = (state = { products: [] }, action) => {
   }
 };
 
-// SINGLE PRODUCT
+//  PRODUCT CREATE REVIEW..
 export const productDetailsReducer = (
   state = { product: { reviews: [] } },
   action
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { ...state, loading: true };
+      return { loading: true };
     case PRODUCT_DETAILS_SUCCESS:
-      return { loading: false, product: action.payload };
+      return { loading: false, success: true };
     case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+// SINGLE PRODUCT
+export const productCreateReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEWS_REQUEST:
+      return { ...state, loading: true };
+    case PRODUCT_CREATE_REVIEWS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_CREATE_REVIEWS_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_REVIEWS_RESET:
       return { loading: false, error: action.payload };
     default:
       return state;
--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -2,6 +2,7 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import Product from "./../Models/productModel.js";
+import { protect } from "../Middleware/Authmiddleware.js"
 
 const productRoute = express.Router();
 
@@ -21,6 +22,42 @@ productRoute.get(
     const product = await Product.findById(req.params.id);
     if (product) {
       res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("product not found with such id");
+    }
+  })
+);
+
+//  PRODUCT REVEIW
+productRoute.post(
+  "/:id/review",
+  protect,
+  asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body;
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      const alreadyReviewed = product.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      );
+      if (alreadyReviewed) {
+        res.status(400);
+        throw new Error("product can only be reviewed once!");
+      }
+      const review = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+      product.reviews.push(review);
+      product.numReviews = product.reviews.length;
+      product.rating =
+        product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        product.reviews.length;
+      await product.save()
+      res.status(201).json({message:"review added"})
+
     } else {
       res.status(404);
       throw new Error("product not found with such id");
