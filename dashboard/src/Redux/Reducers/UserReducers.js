5ce6cb033e534470dc0048a64fac7e51457e3b79
oladips <oladips200@gmail.com>
2022-03-05 22:34:36 +0000

    logged admin in succesfully

--- dashboard/src/Redux/Reducers/UserReducers.js
+++ dashboard/src/Redux/Reducers/UserReducers.js
@@ -21,4 +21,3 @@ export const userLoginReducer = (state = {}, action) => {
       return state;
   }
 };
-
--- dashboard/src/screens/LoginScreen.js
+++ dashboard/src/screens/LoginScreen.js
@@ -1,13 +1,38 @@
-import React from "react";
+/** @format */
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../Redux/Actions/UserActions";
+import Message from "../components/LoadingError/Error";
+import Loading from "../components/LoadingError/Loading";
 
-const Login = () => {
+const Login = ({ location, history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, loading, userInfo } = userLogin;
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
   return (
     <>
       <div
         className="card shadow mx-auto"
         style={{ maxWidth: "380px", marginTop: "100px" }}
       >
-        <div className="card-body">
+        {error && <Message variant="alert-danger">{error}</Message>}
+        {loading && <Loading />}
+        <div className="card-body" onSubmit={submitHandler}>
           <h4 className="card-title mb-4 text-center">Sign in</h4>
           <form>
             <div className="mb-3">
@@ -15,6 +40,8 @@ const Login = () => {
                 className="form-control"
                 placeholder="Email"
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="mb-3">
@@ -22,6 +49,8 @@ const Login = () => {
                 className="form-control"
                 placeholder="Password"
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
